46e4ab1eb100f5b07b23cd2df374a52043aaad3c
Heiro Usana <hsusana@student.apc.edu.ph>
2024-01-23 13:59:54 +0800

    edited week 8 file

---
+++ week 8/family.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>My Family Introduction</title>
+<script>
+function goBack() {
+  window.history.back()
+}
+</script>
+</head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia&effect=fire">
+<style>
+body {
+  background-image: url("bg.gif");
+  }
+  
+#h1 {
+    border-style:inset;
+    border-color: black;
+	color: white;
+	border-width: 10px;
+	text-align: center;
+	font-style: oblique;
+	font-family: "Sofia", sans-serif;	
+	}
+#h2 {
+   text-align: center;
+   color: white;
+   font-style: italic;
+   font-size: 25px;
+   }
+
+div {
+   float: left;
+   padding: 10px;
+   margin-left: 45px;
+  }
+  
+#div1 {
+   color: white;
+   font-size: 16px;
+  }
+  
+#div2 {
+   border: 10px double white;
+   width: 300px;
+   }
+   
+#div3 {
+   color: white;
+   font-size: 16px;
+  }
+  
+#div4 {
+   border: 10px double white;
+   width: 300px;
+   }
+   
+#div5 {
+    color: white;
+    font-size: 16px;
+	margin-top: 40px;
+}
+
+#divone {
+    border: 10px double white;
+    width: 300px;
+	margin-left: 157px;
+	margin-top: 40px
+ }
+ 
+#divtwo {
+   color: white;
+   font-size: 16px;
+   margin-top: 40px;
+  }
+  
+#divtree {
+  border: 10px double white;
+  width: 300px;
+  margin-top: 40px; 
+ }
+  
+   
+#demo {
+  text-align: center;
+  font-size: 25px;
+  color: white;
+  }
+#btn1 {
+  float: left;
+  margin-left: 10px;
+  }
+
+</style>
+<body>
+<h1 id="h1"><b>My Family</b></h1>
+<input id="btn1" type="button" value="Back" onclick="goBack()">
+<p id="demo"></p>
+<script>
+let text = "Happy";
+text += " Family";
+document.getElementById("demo").innerHTML = text;
+</script>
+
+
+<div id="div2"> <img src="father.png" alt="father" width="300" height="300"></div>
+<div id="div1"> <b>This is my father his name is Heinrich Usana, he is 50 years<br>
+old. My father is a soldier in Air Force. He is hardworking, kind,<br>
+kind, helpul and most of all he always puts us first before himself.</b></div>
+
+<div id="div4"><img src="mother.png" alt="mother" width="300px" height="300"></div>
+<div id="div3"> <b>This is my mother her name is Rosabella Usana, she is 50 years<br>
+old. She is kind, hardworking just like my father, delicious<br>
+and a good cook</b></div>
+
+
+<div id="divone"><img src="my sister.png" alt="graduation pic" width="300px" height="300"></div>
+<div id="div5"> <b>This is my eldest sister her name is Rica Rose Usana,  <br>
+she is 22 years old. She works in Cebu Pacific and is <br>
+assigned to the international airport in Pasay. She <br>
+is kind and hardworking just like them mom.</b></div>
+
+<div id="divtree"><img src="youngest sister.png" alt="smart" width="300px" height="300"></div>
+<div id="divtwo"> <b>This is my youngest sister her name is Erich Usana, she is <br>
+17 years old. She is in grade 11 now and his grade is high <br>
+so he gets many awards and he is always included in the<br>
+ honors.</b></div>
+
+
+
+
+
+</body>
+</html>
---
+++ week 8/my pets.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+</head>
+<header>
+   <div id="logo">
+   <img src="dog.gif" alt="ling" style="width:200px;height:200px;"> 
+   </div>
+</header>
+<style>
+body {
+   background-image: url("bg.gif");
+  }
+#h1 {
+  color: white;
+  text-align: center;
+  font-family: "Sofia", sans-serif;	
+  font-size: 40px;
+  }
+#myBtn {
+  float: left;
+  margin-left: 15px;
+  }
+div.gallery {
+  margin: 55px;
+  border: 1px solid #ccc;
+  float: left;
+  width: 280px;
+}
+
+div.gallery:hover {
+  border: 1px solid #777;
+}
+
+div.gallery img {
+  width: 100%;
+  height: auto;
+}
+
+div.desc {
+  padding: 15px;
+  text-align: center;
+  color: white;
+}
+</style>
+<body>
+  <header>
+    <div id="h1">
+      <h1><b>My Pets</b></h1>
+    </div>
+  </header>
+<button id="myBtn">Click it</button>
+
+<script>
+document.getElementById("myBtn").addEventListener("click", function() {
+  alert("Arf Arf!");
+});
+</script>
+<div class="gallery">
+  <a target="_blank" href="lucky.png">
+    <img src="lucky.png" alt="mother dog" width="600" height="400">
+  </a>
+  <div class="desc"><b>Name: Lucky</b></div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="dagul.png">
+    <img src="dagul.png" alt="baby dog" width="600" height="400">
+  </a>
+  <div class="desc"><b>Name: Dagul</b></div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="dina.png">
+    <img src="dina.png" alt="baby dog" width="600" height="400">
+  </a>
+  <div class="desc"><b>Name: Dina</b></div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="oreo.png">
+    <img src="oreo.png" alt="white dog" width="600" height="400">
+  </a>
+  <div class="desc"><b>Name: Oreo</b></div>
+</div>
+</body>
+</html> 
