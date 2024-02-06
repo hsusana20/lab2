22c483776362ff7fc205514968cafc2891b30621
hsusana <152581281+hsusana20@users.noreply.github.com>
2024-02-06 12:21:34 +0800

    Update forms.php

--- week 8/forms.php
+++ week 8/forms.php
@@ -203,8 +203,8 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('$name', '$website', '$email;')";
+$sql = "INSERT INTO hsusana_myguests (name, email, website, message, gender)
+VALUES ('$name', '$email', '$website, '$message', '$gender';')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
